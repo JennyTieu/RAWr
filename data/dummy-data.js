e7c06fbaa57ee77cf3a822eebf5a577a8951d064
Jenny Tieu <jenny_tieu@hotmail.de>
2021-05-21 11:45:06 +0200

    21.05.2021 dummy data update

--- data/dummy-data.js
+++ data/dummy-data.js
@@ -24,4 +24,9 @@ export const REFERENCEITEMS = [
     new ReferenceItem('m3', ['t1', 't9'], 'Over The Garden Wall', false, false, 'Theme Inspo', 'https://www.instagram.com/ayceesky/', './data/artworks/ayceesky/overTheGardenWall.jpg'),
     new ReferenceItem('m4', ['t2', 't9'], 'Overthinking', false, false, '', 'https://www.instagram.com/ayceesky/', './data/artworks/ayceesky/overthinking.jpg'),
     new ReferenceItem('m5', ['t1', 't8', 't9'], 'Red Riding Hood And The Wolf', false, false, 'Theme Inspo', 'https://www.instagram.com/ayceesky/', './data/artworks/ayceesky/redHood.jpg'),
+    new ReferenceItem('m6', ['t1', 't3', 't2', 't9'], 'The Painted Lady', false, false, 'Katara as the painted Lady in Avatar the last Airbender', 'https://www.instagram.com/ayceesky/', './data/artworks/ayceesky/thePaintedLady.jpg'),
+    new ReferenceItem('m7', ['t1', 't4', 't8'], 'Felix (FE)', false, false, 'Felix from Fire Emblem', 'https://www.instagram.com/blueberry_jia/', './data/artworks/blueberry_jia/felix.jpg'),
+    new ReferenceItem('m8', ['t4', 't8', 't10'], 'Noctis (FF)', false, false, 'Noctis from Final Fantasy', 'https://www.instagram.com/blueberry_jia/', './data/artworks/blueberry_jia/noctis.jpg'),
+    new ReferenceItem('m9', ['t3', 't8'], 'Shamir (FE)', false, false, 'Shamir from FireEmblem, Archer Pose', 'https://www.instagram.com/blueberry_jia/', './data/artworks/blueberry_jia/shamir.jpg'),
+    new ReferenceItem('m10', ['t7', 't9'], 'Tears', false, false, 'Monochrome Angel', 'https://www.instagram.com/blueberry_jia/', './data/artworks/blueberry_jia/tears.jpg'),
 ];
