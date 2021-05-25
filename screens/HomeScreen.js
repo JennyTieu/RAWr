e7fe2d540d9b7fce825642cfe6c7cb42d91dce0d
Jasmin <jasmin.seeliger@googlemail.com>
2021-05-26 01:25:07 +0200

    updated SearchBar

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,26 +1,47 @@
-import React, {useContext} from 'react';
+import React, {useContext, useState} from 'react';
 import {StyleSheet, View, TouchableWithoutFeedback, Keyboard, FlatList} from "react-native";
-import {SearchBar} from "react-native-elements";
+import {SearchBar, Button} from "react-native-elements";
 import{ReferenceContext} from '../data/ReferenceContext';
 import GridTile from "../components/GridTile";
+import SearchScreen from "./SearchScreen";
+import {Ionicons} from "@expo/vector-icons";
 
 export default HomeScreen = ({ route, navigation}) => {
 
   const[referenceData] = useContext(ReferenceContext);
   const allReferences = referenceData.referenceItems.filter(item => item.image);
+  const [showSearchScreen, setShowSearchScreen] = useState(false);
 
   const clickHandler = (id) => {
     console.log(id);
   };
 
+  searchHandler = () => {
+    setShowSearchScreen(true);
+  };
+
+  cancelModalHandler = () => {
+    setShowSearchScreen(false);
+  };
+
   return (
     //<TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
     <View>
+      <SearchScreen 
+          visible={showSearchScreen}
+          onCancelModal={cancelModalHandler}
+      />
       <View style={styles.topContainer}>
         <SearchBar 
           placeholder="enter tag ..."
           containerStyle= {styles.searchBar}
           lightTheme="true"
+          onChangeText= {() => {return <SearchScreen />}}
+        />
+        <Button 
+          type="clear"
+          icon={<Ionicons name="md-options" size={30}/>}
+          onPress={searchHandler}
         />
       </View>
       <View style={styles.middleContainer}>
