c4973aefd93b06d0611cb15870992ded43ad526b
Jasmin <jasmin.seeliger@googlemail.com>
2021-05-26 02:06:55 +0200

    added GridTileTags

---
+++ components/GridTileTags.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { StyleSheet, TouchableOpacity, View, Text } from "react-native";
+
+export default GridTileTags = props => {
+  return (
+    <View style={styles.columnWrapper}> 
+      <TouchableOpacity
+        style={styles.itemContainer}
+        onPress={() => props.onClick(props.id)}
+      >
+        <Text style={styles.gridText}>{props.id}</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  itemContainer: {
+    flex: 1,
+    margin: 8,
+    padding: 10,
+    height: 45,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "white",
+    borderRadius: 30,
+  },
+  gridText: {
+    fontSize: 15,
+    fontWeight: 'bold',
+    textAlign: 'center'
+  },
+  columnWrapper: {
+    flex: 1,
+    maxWidth: '50%'
+  }
+});
