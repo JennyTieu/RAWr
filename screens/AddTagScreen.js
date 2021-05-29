9b0b9194903c71bff25ca61a6f5d21aff1e4cd6a
Jasmin <jasmin.seeliger@googlemail.com>
2021-05-29 22:36:25 +0200

    updated AddTagScreen

--- screens/AddTagScreen.js
+++ screens/AddTagScreen.js
@@ -1,10 +1,32 @@
-import React from "react";
+import React, {useContext} from "react";
 import { View, Text } from "react-native";
+import Tag from "../models/tag";
+import { ReferenceContext } from "../data/ReferenceContext";
+import InputTile from "../components/InputTile";
 
 export default TagScreen = ({ route, navigation}) => {
+
+  const [referenceData, setReferenceData] = useContext(ReferenceContext);
+  const {tagId} = referenceData.tags.filter(item => item.id);
+
+  const addHandler = name => {
+    if (tagId == null) {
+      let newIdCounter = referenceData.idCounterTags += 1;
+      let newTags = referenceData.tags;
+      newTags.push(new Tag('t' + newIdCounter, name));
+      setReferenceData(referenceData => ({
+        tags: newTags,
+        referenceItems: referenceData.referenceItems,
+        idCounterTags: newIdCounter,
+        idCounterReferences: referenceData.idCounterReferences
+      }));
+    } 
+    navigation.goBack();
+  };
+
   return (
     <View>
-      <Text>AddTagScreen</Text>
+      <InputTile placeholderText={"enter title ..."} onAdd={addHandler} navigation={navigation}/>
     </View>
   )
 }
